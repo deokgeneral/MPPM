54f15548ce7942200908f794fc22465d78012927
deokgeneral <alsejrrl@naver.com>
2024-01-30 22:09:30 +0900

    new

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './header.css'
-import Hambar from './hambar';
 import { MdOutlinePets } from "react-icons/md";
 
 function Header() {
@@ -13,7 +12,22 @@ function Header() {
                     <li><a href='#'>MBTI 궁합</a></li> 
                     <li><a href='#'>MBTI 롤링페이퍼</a></li>                 
                 </ul>
-            <Hambar />
+                    <div className="hambar">
+                        <input type="checkbox" id="icon" />
+                        <label htmlFor="icon"> 
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </label>
+                    <div className='hambar-menu'>
+                        <h2>MENU</h2>
+                        <ul id='hambar-menu_li'>
+                        <li><a href='#'>나의 MBTI</a></li>
+                        <li><a href='#'>MBTI 궁합</a></li>
+                        <li><a href='#'>MBTI 롤링페이퍼</a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     );
